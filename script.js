17c3c29098cf3cde59ed07fba77d9783099915fb
Shivani Jetty <shivanijetty@gmail.com>
2022-10-19 21:32:40 -0400

    add script.js, connect to index.html

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 const h2 = document.createElement("h2");
-h2.textContent = "I bought something and you're gonna be mad..";
+h2.textContent = "My name is Shivani and I'm a coder :) ";
 document.querySelector("body").appendChild(h2);
